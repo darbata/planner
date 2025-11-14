7d610d02b7f77f3c52a3f7ab904ed85d3b0dd97f
Darren Batarina <batarina.darren@gmail.com>
2025-11-14 13:03:09 +1100

    task is draggable

--- src/planner/Task.tsx
+++ src/planner/Task.tsx
@@ -1,15 +1,31 @@
 import './styles/task.css'
-import {useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import type {taskModel} from "../services/firebase/taskModel.ts";
 import {FaRegCircle} from "react-icons/fa";
 import {FaRegCheckCircle} from "react-icons/fa";
+import { draggable } from '@atlaskit/pragmatic-drag-and-drop/element/adapter';
 
 export function Task({task} : {task: taskModel})  {
+
+    const ref = useRef(null);
+    const [dragging, setDragging] = useState<boolean>(false);
+
+    useEffect(() => {
+        const el = ref.current;
+        return draggable({
+            element: el,
+            onDragStart: () => setDragging(true),
+            onDrop: () => setDragging(false),
+        });
+    }, [])
 
     const [complete, setComplete] = useState(task.isComplete);
 
     return (
-        <div className={"task"} >
+        <div
+            className={`task ${dragging && "task--dragging"}`}
+            ref={ref}
+        >
             <div className={`task__description ${complete ? "task__description--complete" : ""}`}>
                 {complete
                     ? <p><s>{task.description}</s></p>
