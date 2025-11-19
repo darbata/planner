b467082e7992d678df69b82c2ee329794ac457b5
Darren Batarina <batarina.darren@gmail.com>
2025-11-19 17:53:56 +1100

    bug fix: empty task list no longer breaks UI

--- src/planner/Task.tsx
+++ src/planner/Task.tsx
@@ -1,55 +1,16 @@
 import './styles/task.css'
-import {useEffect, useRef, useState} from "react";
+import {useState} from "react";
 import type {taskModel} from "../services/firebase/taskModel.ts";
 import {FaRegCircle, FaRegCheckCircle} from "react-icons/fa";
-import {draggable, dropTargetForElements} from '@atlaskit/pragmatic-drag-and-drop/element/adapter';
-import {combine} from "@atlaskit/pragmatic-drag-and-drop/combine";
 
 export function Task({task} : {task: taskModel})  {
-
-    const ref = useRef(null);
-    const [dragging, setDragging] = useState<boolean>(false);
-
-
-
-
-    useEffect(() => {
-        const el = ref.current;
-        if (!el) return;
-
-        // already returns clean up function
-        return combine(
-
-            draggable({
-                element: el,
-                getInitialData: () => ({task}),
-                onDragStart: () => setDragging(true),
-                onDrop: () => setDragging(false),
-            }),
-
-            dropTargetForElements({
-                element: el,
-                getData: () => ({task}),
-                onDrop: ({source, self, location}) => {
-                    console.log("source")
-                    console.log(source)
-                    console.log("self")
-                    console.log(self)
-                    console.log("location")
-                    console.log(location)
-                }
-            })
-        )
-
-    }, [task])
-
     const [complete, setComplete] = useState(task.isComplete);
 
     return (
         <div
             className={`task `}
         >
-            <div ref={ref}  className={`${dragging && "task__description--dragging"} task__description ${complete ? "task__description--complete" : ""}`}>
+            <div className={`task__description ${complete ? "task__description--complete" : ""}`}>
                 {complete
                     ? <p><s>{task.description}</s></p>
                     : <p>{task.description}</p>
--- src/planner/TaskList.tsx
+++ src/planner/TaskList.tsx
@@ -2,66 +2,24 @@ import type {taskModel} from "../services/firebase/taskModel.ts";
 import {Task} from "./Task.tsx";
 import "./styles/task-list.css"
 import {NewTaskForm} from "./NewTaskForm.tsx";
-import {useContext, useEffect, useRef, useState} from "react";
-import {dropTargetForElements} from "@atlaskit/pragmatic-drag-and-drop/element/adapter";
-import {TasksDispatchContext} from "./TasksProvider.tsx";
 
 export function TaskList({date, tasks} : {
     tasks: taskModel[],
     date: string
 }) {
 
-    const ref = useRef(null);
-    const [isDraggedOver, setIsDraggedOver] = useState(false);
-
-    const dispatch = useContext(TasksDispatchContext);
-
-    useEffect(() => {
-        const el = ref.current;
-        if (!el) return;
-
-        return dropTargetForElements({
-            element: el,
-            onDragEnter: () => setIsDraggedOver(true),
-            onDragLeave: () => setIsDraggedOver(false),
-            getData: () => ({date: date}),
-            onDrop: ({source, self, location}) => {
-                const innermost  = location.current.dropTargets[0];
-
-                // dropped on element inside task list
-                if (self.element !== innermost.element) {
-                    console.log("Dropped on element inside task list");
-                    return
-                }
-
-                console.log(self);
-
-                // else dropped on task list
-                const taskData = source.data as { task: taskModel };
-                const taskListData = self.data as { date: string }
-
-                dispatch({
-                    type: "APPEND_TASK",
-                    payload: {
-                        newDate: taskListData.date,
-                        task: taskData.task,
-                    }
-                })
-            },
-        });
-    }, []);
 
     return (
-        <div ref={ref} className={`task-list ${isDraggedOver && "task-list--dragged-over"}`} >
+        <div className={`task-list`} >
             <ul className={"task_list__entries"}>
-                {tasks
-                    .sort((a, b) => a.order - b.order)
+                {tasks ? tasks
                     .map(task =>
                         <div key={task.id} className={"task-list__entry"}>
                             <Task key={task.id} task={task}/>
                             <hr/>
                         </div>
                     )
+                     : null
                 }
                 <div className={"task-list__entry"}>
                     <NewTaskForm date={date} />
